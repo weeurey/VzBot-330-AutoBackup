1d1a28f49fd317f23d8e64a42b62bec2849a8712
Vz-250 <pi@raspberrypi-KlipperBackupModule>
2025-03-03 16:26:21 +0000

    New backup from 03/03/25 - 16:26:20

---
+++ printer_data/config/updater.py
@@ -0,0 +1,6 @@
+import os
+
+print('Updating Mainsail...')
+os.system('/usr/bin/python /home/pi/mainsail.py')
+print('Mainsail successfully updated!')
+print('Reload this page for the update to take effect.')
